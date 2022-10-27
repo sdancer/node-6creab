7b0d640717154c68b95cf351135fea27f7fb5ef7
shadow dancer <ktion23@gmail.com>
2022-10-27 22:51:49 +0800

    .

--- gleam-wasm/gleam_wasm_wc.js
+++
@@ -1,2 +0,0 @@
-import * as wasm from "./gleam_wasm_wc_bg.wasm";
-export * from "./gleam_wasm_wc_bg.js";
--- gleam-wasm/snippets/gleam-wasm-wc-ee8ec841b455ece1/files.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-export function is_file(path) {
-    console.log("[js] is_file" + path);
-    return true;
-}
-
